a81f6e47cee318160f2e9e1debe221966502b89d
Mb4y4 <d.sommy@gmail.com>
2023-11-02 23:44:17 +0300

    api on search

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -8,8 +8,24 @@ const axiosCreate=axios.create({
 const getGenreList=axiosCreate.get('/genres?key='+key);
 const getAllGames=axiosCreate.get('/games?key='+key);
 const getGameListByGenreId=(id)=>axiosCreate.get('/games?key='+key+'&genres='+id)
+// const searchGames = (searchTerm) => axiosCreate.get('/games?key=' + key + '&search=' + searchTerm);
+
+const searchGames = (searchTerm) => {
+    return axiosCreate.get('/games?key=' + key + '&search=' + searchTerm)
+      .then(response => {
+        // Handle the search results here
+        return response.data; // You may want to return the data for further processing
+      })
+      .catch(error => {
+        // Handle any errors here
+        console.error("Error searching for games:", error);
+        throw error; // You can rethrow the error or handle it as needed
+      });
+
+    }
 export default{
     getGenreList,
     getAllGames,
-    getGameListByGenreId
+    getGameListByGenreId,
+    searchGames
 }
